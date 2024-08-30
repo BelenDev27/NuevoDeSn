0301e549ab42a610a89f16cf86430d79e4345c36
BelenDev27 <166319240+BelenDev27@users.noreply.github.com>
2024-08-30 20:18:50 -0300

    Add files via upload
    tarea9

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+let nombre;
+let edad;
+let ListaNombreDeAlumnos
+
+let numero=31;
+let res = (numero+10) * 2;
+console.log(res);
+
+function calcularAreaCirculo(radio) {
+    if (radio < 0)}
+const radio = 5;
+const area = calcularAreaCirculo(radio);
+console.log
+
